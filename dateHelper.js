5c0708fe701fe801dfdae69b1c61659364526bfa
omega235 <general.of.omega@googlemail.com>
2024-12-15 23:44:12 +0100

    Update auf ESLint9

--- dateHelper.js
+++ dateHelper.js
@@ -1,64 +1,80 @@
-"use strict";
+'use strict';
 
-const monthnames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-
+const monthnames = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+];
 
 class DateHelper {
+    static Padded(topad) {
+        return String(topad).padStart(2, '0');
+    }
 
+    /**
+     * Returns 'HH:mm'
+     *
+     * @param date
+     * @returns
+     */
+    static GetTime(date) {
+        return `${this.Padded(date.getHours())}:${this.Padded(date.getMinutes())}`;
+    }
 
-  static Padded(topad) {
-    return String(topad).padStart(2, "0");
-  }
+    /**
+     * Returns 'DD'
+     *
+     * @param date
+     * @returns
+     */
+    static GetDateNumber(date) {
+        return this.Padded(date.getDate());
+    }
+    /**
+     * Returns 'MM'
+     *
+     * @param date
+     * @returns
+     */
+    static GetMonthNumber(date) {
+        return this.Padded(date.getMonth() + 1);
+    }
+    /**
+     * Returns 'MMMM' (January, February ...)
+     *
+     * @param date
+     * @returns
+     */
+    static GetMonthName(date) {
+        return monthnames[date.getMonth()];
+    }
+    /**
+     * Returns 'MM'
+     *
+     * @param monthnumber
+     * @returns
+     */
+    static GetMonthNamefromNumber(monthnumber) {
+        return monthnames[monthnumber];
+    }
 
-  /**
-   * Returns 'HH:mm'
-   * @param {Date} date
-   * @returns {string}
-   */
-  static GetTime(date) {
-    return this.Padded(date.getHours()) + ":" + this.Padded(date.getMinutes());
-  }
-
-  /**
-   * Returns 'DD'
-   * @param {Date} date
-   * @returns {string}
-   */
-  static GetDateNumber(date) {
-    return this.Padded(date.getDate());
-  }
-  /**
-   * Returns 'MM'
-   * @param {Date} date
-   * @returns {string}
-   */
-  static GetMonthNumber(date) {
-    return this.Padded(date.getMonth() + 1);
-  }
-  /**
-   * Returns 'MMMM' (January, February ...)
-   * @param {Date} date
-   * @returns {string}
-   */
-  static GetMonthName(date) {
-    return monthnames[date.getMonth()];
-  }
-  /**
-   * Returns 'MM'
-   * @param {Number} monthnumber
-   * @returns {string}
-   */
-  static GetMonthNamefromNumber(monthnumber) {
-    return monthnames[monthnumber];
-  }
-
-  /**
-   * Returns 'DD.MM.YYYY'
-   * @param {Date} date
-   * @returns {string}
-   */
-  static GetGermanDate(date) {
-    return `${DateHelper.GetDateNumber(date)}.${DateHelper.GetMonthName(date)}.${date.getFullYear()}`;
-  }
+    /**
+     * Returns 'DD.MM.YYYY'
+     *
+     * @param date
+     * @returns
+     */
+    static GetGermanDate(date) {
+        return `${DateHelper.GetDateNumber(date)}.${DateHelper.GetMonthName(date)}.${date.getFullYear()}`;
+    }
 }
 module.exports = DateHelper;
